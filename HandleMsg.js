0653149eca91434004424ce7dbda760ff90d8571
oalahbro <epradana15@gmail.com>
2022-10-07 21:59:29 +0700

    bkp

--- HandleMsg.js
+++ HandleMsg.js
@@ -1575,6 +1575,7 @@ const HandleMsg = async (message, browser, client = new Client()) => {
                     if (!isUrl(urls)) { return reply('Maaf, link yang kamu kirim tidak valid.') }
                     let xapi = `https://api-xcoders.site/api/download/xnxx?url=${urls}&apikey=5XQ3y7u5zg` 
                     //5XQ3y7u5zg
+                    //7iyNa0qA
                     sendText(resMsg.wait)
                     let { data: hasil } = await get(xapi).catch(e => { return printError(e) })
                     try{
@@ -1603,7 +1604,41 @@ const HandleMsg = async (message, browser, client = new Client()) => {
                     }
                     break
                 }
-
+                case 'tik': {
+                    if (args.length === 0 && !isQuotedChat) return reply(`Masukkan urlnya kakak || Contoh : "https://www.tiktok.com/@0alahbro/video/7149829677396315419"`)
+                    let urls = isQuotedChat ? quotedMsg.body : arg
+                    if (!isUrl(urls)) { return reply('Maaf, link yang kamu kirim tidak valid.') }
+                    let xapi = `https://api-xcoders.site/api/download/tiktok2?url=${urls}&apikey=5XQ3y7u5zg` 
+                    //5XQ3y7u5zg
+                    //7iyNa0qA
+                    sendText(resMsg.wait)
+                    let { data: hasil } = await get(xapi).catch(e => { return printError(e) })
+                    try{
+                        let link = hasil.result.url
+                        const generateRandomString = (myLength) => {
+                            const chars =
+                                "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
+                            const randomArray = Array.from(
+                                { length: myLength },
+                                (v, k) => chars[Math.floor(Math.random() * chars.length)]
+                            );
+                            const randomString = randomArray.join("");
+                            return randomString;
+                        };
+                        let nama = generateRandomString(10)
+                        const response = await fetch(link);
+                        const buffer = await response.buffer();
+                        fs.writeFile('./src/' + nama + '.mp4', buffer, () => {
+                            let path = `./src/${nama}.mp4`
+                            client.sendFile(from, path, '', '', id).catch(e => { return printError(e) })
+                                if (existsSync(path)) unlinkSync(path)
+                        });
+                    }     
+                    catch(err){
+                        return reply("apikey sudah habis 😔")
+                    }
+                    break
+                }
                 case 'fbdl':
                 case 'twdl': {
                     if (args.length === 0 && !isQuotedChat && command == 'fbdl') return reply(`Download Facebook video post. How?\n${prefix}fbdl (alamat video Facebook)\nTanpa tanda kurung`)
